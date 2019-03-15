b97519679c04faaf306ee09334dff6ae9cc7268a
aroberts2603 <aroberts2603@gmail.com>
2019-03-15 11:33:31 -0600

    added auth form

---
+++ main.js
@@ -0,0 +1,26 @@
+document.getElementById("hero-link").onclick = function() {
+	window.location.href = "march-madness.html"
+}
+
+document.getElementById("open-login").onclick = function() {
+	document.getElementById("login-dropdown").style.height = "167px";
+}
+
+document.getElementById("close-login").onclick = function() {
+	document.getElementById("login-dropdown").style.height = "0px";
+
+}
+
+class BracketViewer {
+	constructor() {
+
+	}
+
+	createHTML() {
+
+	}
+
+	updateViewer() {
+
+	}
+}
