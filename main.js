1b7b020d191fa2b08bb7f002bb2740d9760ff3f3
Alex R <41028453+aroberts2603@users.noreply.github.com>
2019-03-15 16:03:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyD0eyh7SC-idMvrTBUNbh7xuCI6yeulT18",
+	authDomain: "grandview-brackets.firebaseapp.com",
+	databaseURL: "https://grandview-brackets.firebaseio.com",
+	projectId: "grandview-brackets",
+	storageBucket: "grandview-brackets.appspot.com",
+	messagingSenderId: "547379249468"
+};
+firebase.initializeApp(config);
+
 document.getElementById("hero-link").onclick = function() {
 	window.location.href = "march-madness.html"
 }
@@ -10,6 +21,8 @@ document.getElementById("close-login").onclick = function() {
 	document.getElementById("login-dropdown").style.height = "0px";
 
 }
+
+
 
 class BracketViewer {
 	constructor() {
